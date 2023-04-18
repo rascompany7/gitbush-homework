9afee69cf46094e2634072e838d1f12f1f6dd16a
rascompany7 <81210142+rascompany7@users.noreply.github.com>
2023-04-18 17:40:02 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,9 +1,25 @@
-#!/bin/bash
-cd "one_3"
+Доп задание*
+Send an http request to the server. http://162.55.220.72:5005/terminal-hw-request
+пишем curl http://162.55.220.72:5005/terminal-hw-request
 
-mkdir d_1 d_2
+Write a script that will automatically execute steps 3, 4, 5, 6, 7, 8, 13
 
-cd d_1
+3) cd 
+
+4) mkdir book_1 book_2 book_3
+
+5) cd book_1
+
+6)touch 1.txt 2.txt 3.txt 4.json 5.json
+
+7)mkdir book_4 book_5 book_6
+
+8)ls -la 
+
+13)mv 1.txt/book_1 2.txt/book_2
+
+./myscript.sh
+
 
  
 
